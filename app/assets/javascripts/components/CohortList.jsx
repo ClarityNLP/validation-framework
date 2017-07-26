f84b1a2484eaf3fc538ad128f0bd303d6a960c65
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2017-07-26 10:11:49 -0400

    more checks on cohorts

--- app/assets/javascripts/components/CohortList.jsx
+++ app/assets/javascripts/components/CohortList.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const AnnotationSetButton = (props) => {
-  let btn =
-  <div className="btn-group">
-    <button type="button" className="btn btn-success btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      Annotate <span className="caret"></span>
-    </button>
+    let btn =
+        <div className="btn-group">
+            <button type="button" className="btn btn-success btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Annotate <span className="caret"></span>
+            </button>
 
-    <ul className="dropdown-menu">
-      {props.cohort.annotationSets.map(set => (
-        <li key={set.name} {...set}><a href={set.url}>{set.name}</a></li>
-      ))}
-    </ul>
-  </div>;
-  return (btn);
+            <ul className="dropdown-menu">
+                {props.cohort.annotationSets.map(set => (
+                    set !== null && set.name ?
+                        <li key={set.name} {...set}><a href={set.url}>{set.name}</a></li> : <li/>
+                ))}
+            </ul>
+        </div>;
+    return (btn);
 };
 
 const AdminButton = (props) => {
@@ -25,7 +26,9 @@ const AdminButton = (props) => {
 
             <ul className="dropdown-menu">
                 {props.admin.map(a => (
-                    <li key={a.name} {...a}><a href={'/download_result/' + a.annotation_set_id}>{a.name}</a></li>
+                    a !== null && a.name ?
+                        <li key={a.name} {...a}><a href={'/download_result/' + a.annotation_set_id}>{a.name}</a></li> :
+                        <li/>
                 ))}
             </ul>
         </div>;
@@ -33,88 +36,88 @@ const AdminButton = (props) => {
 };
 
 class CohortList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        cohorts : []
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.cohorts && nextProps.cohorts.length !== this.state.cohorts.length) {
-        const newCohorts = nextProps.cohorts.filter((c) => {
-            let isAdmin = false;
-            const admin = nextProps.adminCohorts[c.id];
-            if (admin) {
-                admin.map((a) => {
-                    if (a.cohort_source === c.cohort_type) {
-                        isAdmin = true;
-                    }
-                    return null;
-                });
-            }
-            return c.annotationSets.length > 0 || isAdmin;
-        });
-        this.setState({
-            cohorts : newCohorts
-        })
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            cohorts : []
+        };
     }
-  }
 
-  render() {
-
-    return (
-      <div className="row" style={{padding:"0px 20px"}}>
-        <table className="table table-striped table-hover">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Annotate</th>
-              <th>View</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Date</th>
-              <th>Admin</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.cohorts.map((cohort) => {
-              const admin = this.props.adminCohorts[cohort.id];
-              return (
-
-                <tr key={cohort.id} onClick={() => {window.location.url = cohort.viewUrl}} style={{
-                  verticalAlign : "middle",
-                  cursor: "pointer"
-                }}>
-                  <td>{cohort.id}</td>
-                  <td>
-                      {cohort.annotationSets.length > 0 ?
-                          <AnnotationSetButton cohort={cohort} />
-                          : <div></div>
-                      }
-                  </td>
-                  <td>
-                    <a className="btn btn-xs btn-primary" href={cohort.viewUrl}>View</a>
-                  </td>
-                  <td>{cohort.name}</td>
-                  <td>{cohort.description}</td>
-                  <td>{cohort.createdDate}</td>
-                  <td>
-                      {admin && admin.length > 0 ?
-                          <AdminButton cohort={cohort} admin={admin} />
-                          : <div></div>
-                      }
-                  </td>
-              </tr>
-              )
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.cohorts && nextProps.cohorts.length !== this.state.cohorts.length) {
+            const newCohorts = nextProps.cohorts.filter((c) => {
+                let isAdmin = false;
+                const admin = nextProps.adminCohorts[c.id];
+                if (admin) {
+                    admin.map((a) => {
+                        if (a.cohort_source === c.cohort_type) {
+                            isAdmin = true;
+                        }
+                        return null;
+                    });
+                }
+                return c.annotationSets.length > 0 || isAdmin;
+            });
+            this.setState({
+                cohorts : newCohorts
             })
-           }
-          </tbody>
-        </table>
-      </div>
-    );
-  };
+
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="row" style={{padding:"0px 20px"}}>
+                <table className="table table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Annotate</th>
+                        <th>View</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Date</th>
+                        <th>Admin</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.cohorts.map((cohort) => {
+                        const admin = this.props.adminCohorts[cohort.id];
+                        return (
+
+                            <tr key={cohort.id} onClick={() => {window.location.url = cohort.viewUrl}} style={{
+                                verticalAlign : "middle",
+                                cursor: "pointer"
+                            }}>
+                                <td>{cohort.id}</td>
+                                <td>
+                                    {cohort.annotationSets && cohort.annotationSets.length > 0 ?
+                                        <AnnotationSetButton cohort={cohort} />
+                                        : <div></div>
+                                    }
+                                </td>
+                                <td>
+                                    <a className="btn btn-xs btn-primary" href={cohort.viewUrl}>View</a>
+                                </td>
+                                <td>{cohort.name}</td>
+                                <td>{cohort.description}</td>
+                                <td>{cohort.createdDate}</td>
+                                <td>
+                                    {admin && admin.length > 0 ?
+                                        <AdminButton cohort={cohort} admin={admin} />
+                                        : <div></div>
+                                    }
+                                </td>
+                            </tr>
+                        )
+                    })
+                    }
+                    </tbody>
+                </table>
+            </div>
+        );
+    };
 }
 
 export default CohortList;
